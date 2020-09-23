6b04d09ebdf278b2791199f1818a515da85af053
Quoc Hung <51414190+qhung312@users.noreply.github.com>
2020-09-23 22:35:26 +0700

    Create short.cpp

---
+++ short.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,a,n) for (int i=a;i<int(n);i++)
+#define per(i,a,n) for (int i=int(n)-1;i>=0;i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define SZ(x) ((int)(x).size())
+#define dbg(x) #x << ": " << x << " "
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<pi> vpi;
+typedef vector<int> vi;
+typedef pair<ll, ll> pl;
+typedef vector<ll> vl;
+typedef vector<pl> vpl;
+typedef long double ld;
+typedef pair<double, double> pd;
+typedef vector<double> vd;
+typedef vector<pd> vpd;
+
+int main() {
+	ios_base::sync_with_stdio(false),cin.tie(0);
+	return 0;
+}
