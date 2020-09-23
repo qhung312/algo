3339587a50e3c6b8c0d59b25910c653be82b18f8
Quoc Hung <51414190+qhung312@users.noreply.github.com>
2020-09-23 22:37:50 +0700

    Create full.cpp

---
+++ full.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,a,b) for (int i = a; i < int(b); i++)
+#define per(i,a,b) for (int i = int(b)-1; i >= a; i--)
+#define trav(a, x) for (auto &a : x)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define SZ(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+int main() {
+	ios_base::sync_with_stdio(false), cin.tie(nullptr);
+	return 0;
+}
