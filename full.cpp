783a9115d33ad5d63e603e7e6756498eab342f19
Quoc Hung <51414190+qhung312@users.noreply.github.com>
2020-09-24 14:09:59 +0700

    Update full.cpp

--- full.cpp
+++ full.cpp
@@ -6,10 +6,14 @@ using namespace std;
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define SZ(x) (int)(x).size()
+#define dbg(x) #x << ": " << x << " "
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef vector<ll> vll;
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep; for (const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename T, size_t size> ostream& operator<<(ostream &os, const array<T, size> &arr) { os << '{'; string sep; for (const auto &x : arr) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
 
 int main() {
 	ios_base::sync_with_stdio(false), cin.tie(nullptr);
