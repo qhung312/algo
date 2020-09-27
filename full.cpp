df7169c7d59c7eb462554b85dc404e524078fd68
Quoc Hung <51414190+qhung312@users.noreply.github.com>
2020-09-27 15:12:16 +0700

    Update full.cpp

--- full.cpp
+++ full.cpp
@@ -1,21 +1,44 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-#define rep(i,a,b) for (int i = a; i < int(b); i++)
-#define per(i,a,b) for (int i = int(b)-1; i >= a; i--)
-#define trav(a, x) for (auto &a : x)
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define SZ(x) (int)(x).size()
-#define dbg(x) #x << ": " << x << " "
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep; for (const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
-template<typename T, size_t size> ostream& operator<<(ostream &os, const array<T, size> &arr) { os << '{'; string sep; for (const auto &x : arr) os << sep << x, sep = ", "; return os << '}'; }
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+template<class T>
+ostream& operator<<(ostream &os, const vector<T> &v) {
+  os << "{";
+  bool f = true;
+  for (int i = 0; i < v.size(); i++) {
+    if (!f) {
+      os << ", ";
+    } else {
+      f = false;
+    }
+    os << v[i];
+  }
+  return os << "}";
+}
+
+template<typename A, typename B>
+ostream& operator<<(ostream &os, const pair<A, B> p) {
+  return os << "(" << p.first << ", " << p.second << ")";
+}
+
+string change_bit(int n, int base) {
+  string s = "";
+  while (n > 0) {
+    s += char(n % base) + '0';
+    n /= base;
+  }
+  reverse(s.begin(), s.end());
+  return s;
+}
+
+#define dbg(x) #x << ": " << (x) << " "
 
 int main() {
-	ios_base::sync_with_stdio(false), cin.tie(nullptr);
-	return 0;
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  return 0;
 }
