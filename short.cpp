b02ce4cad8a525711b85f27cd65325583deb8d1f
Quoc Hung <51414190+qhung312@users.noreply.github.com>
2020-09-30 20:32:06 +0700

    Update short.cpp

--- short.cpp
+++ short.cpp
@@ -3,10 +3,10 @@ using namespace std;
 template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep; for (const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
 template<typename T, size_t size> ostream& operator<<(ostream &os, const array<T, size> &arr) { os << '{'; string sep; for (const auto &x : arr) os << sep << x, sep = ", "; return os << '}'; }
 template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-#define rep(i,a,n) for (int i=a;i<int(n);i++)
-#define per(i,a,n) for (int i=int(n)-1;i>=0;i--)
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
+#define rep(i,a,n) for(int i=a;i<int(n);i++)
+#define per(i,a,n) for(int i=int(n)-1;i>=0;i--)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
 #define fi first
 #define se second
 #define pb push_back
@@ -15,18 +15,18 @@ template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, 
 #define SZ(x) ((int)(x).size())
 #define dbg(x) #x << ": " << x << " "
 typedef long long ll;
-typedef pair<int, int> pi;
+typedef pair<int,int> pi;
 typedef vector<pi> vpi;
 typedef vector<int> vi;
-typedef pair<ll, ll> pl;
+typedef pair<ll,ll> pl;
 typedef vector<ll> vl;
 typedef vector<pl> vpl;
 typedef long double ld;
-typedef pair<double, double> pd;
+typedef pair<double,double> pd;
 typedef vector<double> vd;
 typedef vector<pd> vpd;
 
-int main() {
+int main(){
 	ios_base::sync_with_stdio(false),cin.tie(0);
 	return 0;
 }
