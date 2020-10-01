323817399ae16917f34bbe8a79c42b54cdb8d76c
Quoc Hung <51414190+qhung312@users.noreply.github.com>
2020-10-01 15:17:48 +0700

    Update short.cpp

--- short.cpp
+++ short.cpp
@@ -3,8 +3,8 @@ using namespace std;
 template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep; for (const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
 template<typename T, size_t size> ostream& operator<<(ostream &os, const array<T, size> &arr) { os << '{'; string sep; for (const auto &x : arr) os << sep << x, sep = ", "; return os << '}'; }
 template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-#define rep(i,a,n) for(int i=a;i<int(n);i++)
-#define per(i,a,n) for(int i=int(n)-1;i>=0;i--)
+#define rep(i,a,n) for (int i=a;i<int(n);i++)
+#define per(i,a,n) for (int i=int(n)-1;i>=0;i--)
 #define all(x) (x).begin(),(x).end()
 #define rall(x) (x).rbegin(),(x).rend()
 #define fi first
@@ -26,7 +26,7 @@ typedef pair<double,double> pd;
 typedef vector<double> vd;
 typedef vector<pd> vpd;
 
-int main(){
+int main() {
 	ios_base::sync_with_stdio(false),cin.tie(0);
 	return 0;
 }
