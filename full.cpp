5bc7b67c349471abb46877b478f1c9e96df919bf
Quoc Hung <51414190+qhung312@users.noreply.github.com>
2020-10-02 22:09:35 +0700

    Update full.cpp

--- full.cpp
+++ full.cpp
@@ -3,31 +3,9 @@ using namespace std;
 using ll = long long;
 using ull = unsigned long long;
 using ld = long double;
-
-template<class T>
-ostream& operator<<(ostream &os, const vector<T> &v) {
-	os << "{";
-	bool f = true;
-	for (int i = 0; i < v.size(); i++) {
-		if (!f) os << ", "; else f = false;
-		os << v[i];
-	}
-	return os << "}";
-}
-
-template<typename A, typename B>
-ostream& operator<<(ostream &os, const pair<A, B> p) {
-	return os << "(" << p.first << ", " << p.second << ")";
-}
-
-string change_bit(int n, int base) {
-	string s = "";
-	while (n > 0) {
-		s += char(n % base) + '0'; n /= base;
-	} reverse(s.begin(), s.end());
-	return s;
-}
-
+template<class T> ostream& operator<<(ostream &os, const vector<T> &v) { os << "{"; bool f = true; for (int i = 0; i < v.size(); i++) { if (!f) os << ", "; else f = false; os << v[i]; } return os << "}"; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> p) { return os << "(" << p.first << ", " << p.second << ")"; }
+string change_bit(int n, int base) { string s = ""; while (n > 0) { s += char(n % base) + '0'; n /= base; } reverse(s.begin(), s.end()); return s; }
 #define dbg(x) #x << ": " << (x) << " "
 
 int main() {
