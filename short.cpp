715d8b368b6be813131f62755d0f13931a529719
Quoc Hung <51414190+qhung312@users.noreply.github.com>
2020-10-03 23:08:19 +0700

    Update short.cpp

--- short.cpp
+++ short.cpp
@@ -4,8 +4,8 @@ using namespace std;
 template<class T> ostream& operator<<(ostream &os, const vector<T> &v) { os << "{"; bool f = true; for (int i = 0; i < v.size(); i++) { if (!f) os << ", "; else f = false; os << v[i]; } return os << "}"; }
 template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> p) { return os << "(" << p.first << ", " << p.second << ")"; }
 string change_bit(int n, int base) { string s = ""; while (n > 0) { s += char(n % base) + '0'; n /= base; } reverse(s.begin(), s.end()); return s; }
-#define rep(i,a,n) for (int i = a; i < int(n); i++)
-#define per(i,a,n) for (int i = int(n)-1; i >= 0; i--)
+#define FOR(i,a,n) for (int i = a; i < int(n); i++)
+#define ROF(i,a,n) for (int i = int(n)-1; i >= 0; i--)
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define fi first
