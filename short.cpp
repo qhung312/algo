21781d3fe68e7dd88db8b8f6a706552e82211de4
Quoc Hung <51414190+qhung312@users.noreply.github.com>
2020-10-03 22:52:18 +0700

    Update short.cpp

--- short.cpp
+++ short.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep; for (const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
-template<typename T, size_t size> ostream& operator<<(ostream &os, const array<T, size> &arr) { os << '{'; string sep; for (const auto &x : arr) os << sep << x, sep = ", "; return os << '}'; }
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<class T> ostream& operator<<(ostream &os, const vector<T> &v) { os << "{"; bool f = true; for (int i = 0; i < v.size(); i++) { if (!f) os << ", "; else f = false; os << v[i]; } return os << "}"; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> p) { return os << "(" << p.first << ", " << p.second << ")"; }
+string change_bit(int n, int base) { string s = ""; while (n > 0) { s += char(n % base) + '0'; n /= base; } reverse(s.begin(), s.end()); return s; }
 #define rep(i,a,n) for (int i=a;i<int(n);i++)
 #define per(i,a,n) for (int i=int(n)-1;i>=0;i--)
 #define all(x) (x).begin(),(x).end()
